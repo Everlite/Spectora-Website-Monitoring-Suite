0867dcdfa8320d95816ea4dc3c76c7ba7cf989b8
Everlite <realeverlite@gmail.com>
2026-05-22 23:42:26 +0200

    fix(user): align user model with sqlite schema and add name mutator/accessor

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,10 +21,19 @@ class User extends Authenticatable
     protected $fillable = [
         'first_name',
         'last_name',
+        'company_name',
         'email',
         'password',
         'timezone',
         'agency_logo_path',
+        'title',
+        'phone',
+        'mobile_phone',
+        'street',
+        'zip_code',
+        'city',
+        'country',
+        'name',
     ];
 
     /**
@@ -54,6 +63,13 @@ class User extends Authenticatable
     public function getNameAttribute()
     {
         return trim("{$this->first_name} {$this->last_name}");
+    }
+
+    public function setNameAttribute($value)
+    {
+        $parts = explode(' ', trim($value), 2);
+        $this->attributes['first_name'] = $parts[0] ?? '';
+        $this->attributes['last_name'] = $parts[1] ?? '';
     }
 
     public function domains()
