78e50ede1cb9c0ca5b51b313326943b9e64bd739
Everlite <realeverlite@gmail.com>
2026-05-23 00:50:26 +0200

    fix: dynamically force HTTPS scheme based on HTTP_X_FORWARDED_PROTO or app.url config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (str_contains(config('app.url'), 'https://')) {
+        // Erzwinge HTTPS, wenn der Request über einen SSL-Proxy kommt
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        } elseif (str_contains(config('app.url'), 'https://')) {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
     }
