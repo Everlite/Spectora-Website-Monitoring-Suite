09e1fd3b4144d8c8c515f8353fe9ff1f2d945f0c
Everlite <realeverlite@gmail.com>
2026-05-22 23:48:03 +0200

    fix(factory): update definition method exactly as requested in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,9 +28,10 @@ class UserFactory extends Factory
             'last_name' => $this->faker->lastName(),
             'company_name' => $this->faker->company(),
             'email' => $this->faker->unique()->safeEmail(),
-            'password' => static::$password ??= Hash::make('password'),
             'email_verified_at' => now(),
-            'is_admin' => false,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'timezone' => 'UTC',
         ];
     }
 
